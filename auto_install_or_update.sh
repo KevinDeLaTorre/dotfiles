bf19fa81a283d41df447e79c7be447688e7369fe
Kevin De La Torre <kevindelatorre@live.com>
2023-04-06 09:32:51 -0500

    Tweak output of install script

--- auto_install_or_update.sh
+++ auto_install_or_update.sh
@@ -43,21 +43,24 @@ run_copy_to_local_script() {
 
 install_main_programs() {
 	# package managers + os specific
+	echo "== Install main programs =="
 	OS="$(uname)"
 	case $OS in
 	'Linux')
 		OS='Linux'
 		if ! command_exists sudo; then
-			echo "=== running apt-get install -y without sudo (because sudo not installed) ==="
+			echo "|===> running apt-get install -y without sudo (because sudo not installed)\c"
 
 			apt-get install -y zsh sudo wget build-essential procps curl file git rsync vim
+			echo " - Done"
 		else
-			echo "=== installing necessary packages ==="
+			echo "|===> Installing necessary packages\c"
 			sudo apt-get install -y zsh wget build-essential procps curl file git rsync vim
+			echo " - Done"
 		fi
 
 		if ! program_exists oh-my-posh; then
-			echo "=== installing oh-my-posh ==="
+			echo "|===> Installing oh-my-posh\c"
 
 			sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 			sudo chmod +x /usr/local/bin/oh-my-posh
@@ -67,78 +70,88 @@ install_main_programs() {
 			unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 			chmod u+rw ~/.poshthemes/*.omp.*
 			rm ~/.poshthemes/themes.zip
+			echo " - Done"
 		fi
 
 		if ! program_exists vim; then
-			echo "=== installing vim ==="
+			echo "|===> Installing vim\c"
 			sudo apt-get update
 			sudo apt-get install -y vim
+			echo " - Done"
 		fi
 
 		if ! program_exists tmux; then
-			echo "=== installing tmux ==="
+			echo "|===> Installing tmux\c"
 			sudo apt-get update
 			sudo apt-get install -y tmux
+			echo " - Done"
 		fi
 
 		if ! program_exists nvim; then
-			echo "=== installing nvim ==="
+			echo "|===> Installing nvim\c"
 			sudo apt install neovim
+			echo " - Done"
 		fi
 
-		install_all_plugins
-
-		echo "=== copying dotfiles and config files to local ==="
+		echo "|===> Copying dotfiles and config files to local\c"
 		run_copy_to_local_script
+		echo " - Done"
 
 		install_all_plugins
 
-		echo "=== apt-get update + upgrade ==="
+		echo "\n== Update/Upgrade system =="
 		sudo apt-get update
 		sudo apt-get upgrade
+		echo "\n\nDone"
 		;;
 	'Darwin')
 		OS='Mac'
 		if ! command_exists brew; then
-			echo "=== installing brew ==="
+			echo "|===> Installing brew\c"
 			NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+			echo " - Done"
 		fi
 
 		if ! command_exists rsync; then
-			echo "=== installing brew ==="
+			echo "|===> Installing brew\c"
 			brew install rsync
+			echo " - Done"
 		fi
 
 		if ! program_exists oh-my-posh; then
-			echo "=== installing oh-my-posh ==="
+			echo "|===> Installing oh-my-posh\c"
 			brew install jandedobbeleer/oh-my-posh/oh-my-posh
+			echo " - Done"
 		fi
 
 		if ! program_exists tmux; then
-			echo "=== installing tmux ==="
+			echo "|===> Installing tmux\c"
 			brew install tmux
+			echo " - Done"
 		fi
 
 		if ! program_exists vim; then
-			echo "=== installing vim ==="
+			echo "|===> Installing vim\c"
 			brew install vim
+			echo " - Done"
 		fi
 
 		if ! program_exists nvim; then
-			echo "=== installing nvim ==="
+			echo "|===> Installing nvim\c"
 			brew install neovim
+			echo " - Done"
 		fi
 
-		install_all_plugins
-
-		echo "=== copying dotfiles and config files to local ==="
+		echo "|===> Copying dotfiles and config files to local\c"
 		run_copy_to_local_script
+		echo " - Done"
 
 		install_all_plugins
 
-		echo "=== brew update + upgrade ==="
+		echo "\n== Update/Upgrade system =="
 		brew update
 		brew upgrade
+		echo "Done"
 		;;
 	*) ;;
 	esac
@@ -147,57 +160,63 @@ install_main_programs() {
 # Oh-my-zsh
 install_oh_my_zsh() {
 	if ! dir_exists ".oh-my-zsh"; then
-		echo "=== installing oh-my-zsh ==="
+		echo "|----> Installing oh-my-zsh\c"
 		sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+		echo " - Done"
 	fi
 }
 
 # Vundle.vim
 install_vim_plugins() {
 	if ! dir_exists ".vim/bundle/Vundle.vim"; then
-		echo "=== installing vim plugins ==="
+		echo "|----> Installing vim plugins\c"
 		git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 		vim -c ":PluginInstall" -c ":qa"
+		echo " - Done"
 	else
-		echo "=== updating vim plugins ==="
+		echo "|----> Updating vim plugins\c"
 		vim -c ":PluginUpdate" -c ":qa"
+		echo " - Done"
 	fi
 }
 
 # NvChad
 install_nvim_plugins() {
 	if ! dir_exists ".config/nvim"; then
-		echo "=== installing nvchad ==="
+		echo "|----> Installing nvchad\c"
 		git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+		echo " - Done"
 	fi
 
-	echo "=== installing/updating nvim plugins ==="
+	echo "|----> Installing/Updating nvim plugins\c"
 	nvim --headless +PackerSync +qa
+	echo " - Done"
 }
 
 # Tmux
 install_tmux_plugins() {
 	if ! dir_exists ".tmux/plugins/tpm"; then
-		echo "=== installing tmux plugins ==="
+		echo "|----> Installing tmux plugins\c"
 		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 		#   install the plugins
 		sh ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+		echo " - Done"
 	else
-		echo "=== updating tmux plugins ==="
+		echo "|----> Updating tmux plugins\c"
 
 		# update all plugins
 		sh ~/.tmux/plugins/tpm/scripts/update_plugin.sh all
+		echo " - Done"
 	fi
 }
 
 make_missing_dirs() {
-	echo "=== creating missing directories ==="
 	mkdir -p ~/.zsh/personal ~/.zsh/work ~/.zsh/scripts
 }
 
 install_all_plugins() {
-	echo "=== installing all plugins ==="
+	echo "|===> Installing/Updating all plugins"
 	install_oh_my_zsh
 	install_vim_plugins
 	install_nvim_plugins
